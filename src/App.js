acb7dffa39ee20e602a373e880c6fcd8c7b522c1
Lovepreet Kaur <love.174.preetkaur@gmail.com>
2024-04-16 12:26:13 +0530

    Create the Header Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
