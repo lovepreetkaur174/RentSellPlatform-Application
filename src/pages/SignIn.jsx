dbb6b6f02220bd90ed2d320662c6a4272311124b
Lovepreet Kaur <love.174.preetkaur@gmail.com>
2024-04-17 09:03:18 +0530

    Sign in page completed

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import signin from './../Assets/signinbg.jpg'
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import {Link} from 'react-router-dom'
+import OAuth from '../components/OAuth';
 export default function SignIn() {
     const [showPassword,setShowPassword]=useState(false);
     const [formdata,setFormdata]=useState(
@@ -28,18 +32,52 @@ export default function SignIn() {
              <form >
                <input className='w-full px-4 py-2
                text-xl text-gray-700 bg-white
-               border-gray-300 rounded transition ease-in-out' type='email' id='email' value={email}
+               border-gray-300 rounded transition ease-in-out mb-6' type='email' id='email' value={email}
                onChange={onchange}
                placeholder='Email Address'
                />
-               <div>
+               <div className='relative'>
                <input className='w-full px-4 py-2
                text-xl text-gray-700 bg-white
-               border-gray-300 rounded transition ease-in-out' type='password' id='email' value={email}
+               border-gray-300 rounded transition ease-in-out mb-6'
+                type={showPassword ? 'text' : 'password'} id='password' value={password}
                onChange={onchange}
                placeholder='Password'
                />
+               {showPassword ? <FaEyeSlash className='absolute  right-3 top-3 text-xl cursor-pointer'
+               onClick={()=>setShowPassword((prevState)=>!prevState)}/> : <FaEye className='absolute right-3 top-3 text-xl cursor-pointer'
+               onClick={()=>setShowPassword((prevState)=>!prevState)}/>}
                </div>
+               <div>
+                <div className='flex justify-between whitespace-nowrap
+                 text-sm  sm:text-lg'>
+                  <p className='mb-6'>Don't have an account?
+                    <Link to='/sign-up'
+                    className='text-red-600 hover:text-red-700
+                    transition duration-200 ease-in-out
+                    ml-4'>Register</Link>
+                  </p>
+                  <p>
+                    <Link to='/forgot-password'
+                     className='text-blue-600 hover:text-blue-800
+                     transition duration-200 ease-in-out
+                     '>Forgot password?</Link>
+                  </p>
+                </div>
+               </div>
+               <button className='w-full
+               bg-blue-600 text-white px-7
+               py-3 font-medium uppercase rounded shadow-md
+               hover:text-white-700 transition duration-150
+                ease-in-out hover:shadow-md active:bg-blue-800'
+               type='submit'>Sign in</button>
+               <div className='flex  items-center my-4 before:border-t  before:flex-1
+               after:border-gray-300
+               after:border-t  after:flex-1
+               before:border-gray-300'>
+                <p className='text-center font-semibold mx-4'>OR</p>
+               </div>
+               <OAuth/>
              </form>
            </div>
         </div>
