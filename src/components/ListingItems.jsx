a61a4cd7c4d2708772938a020618c28486799cfe
Ayush Duple <ayush.duple@gmail.com>
2024-05-02 16:38:54 +0530

    Map issue

--- src/components/ListingItems.jsx
+++ src/components/ListingItems.jsx
@@ -1,4 +1,4 @@
-import Moment from 'react-moment'
+// import Moment from 'react-moment'
 import {Link} from 'react-router-dom'
 import { MdLocationOn } from "react-icons/md";
 import { FaTrash } from "react-icons/fa";
@@ -11,16 +11,16 @@ export default function ListingItems({listing, id , onEdit , onDelete}) {
             <img className="h-[180px] w-full object-cover 
             hover:scale-105 transition-scale duration-200 ease-in"
             loading='lazy' src={listing.imgUrls[0]} alt="" /> 
-            <Moment  className="absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg" fromNow>
+            {/* <Moment  className="absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg" fromNow>
                 {listing.timestamp?.toDate()}
-            </Moment>
+            </Moment> */}
             <div className="w-full p-[10px]">
                 <div className="flex items-center space-x-1">
-                <MdLocationOn className="h-4 w-4 text-green-600" />
+                <MdLocationOn className="w-4 h-4 text-green-600" />
                 <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">
                 {listing.address}</p>
                 </div>
-                <p  className="font-semibold m-0 text-xl truncate">{listing.name}</p>
+                <p  className="m-0 text-xl font-semibold truncate">{listing.name}</p>
                 <p className="text-[#457b9d] mt-2 font-semibold">$ {listing.offer
               ? listing.discountedPrice
                   .toString()
@@ -31,10 +31,10 @@ export default function ListingItems({listing, id , onEdit , onDelete}) {
             {listing.type === "rent" && " / month"}</p>
               <div className="flex items-center mt-[10px] space-x-3">
                 <div className="flex items-center space-x-1">
-                    <p className="font-bold text-xs">{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed" }</p>
+                    <p className="text-xs font-bold">{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed" }</p>
                 </div>
                 <div className="flex items-center space-x-1">
-                    <p className="font-bold text-xs">{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath" }</p>
+                    <p className="text-xs font-bold">{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath" }</p>
                 </div>
               </div>
             </div>
@@ -44,8 +44,7 @@ export default function ListingItems({listing, id , onEdit , onDelete}) {
          cursor-pointer text-red-500 ' onClick={()=>onDelete(listing.id)}/>
        )}
        {onEdit && (
-        < MdEdit className='absolute bottom-2 right-7 h-4
-         cursor-pointer ' onClick={()=>onEdit(listing.id)}/>
+        < MdEdit className='absolute bottom-2 right-7 h-4 cursor-pointer' onClick={()=>onEdit(listing.id)}/>
        )}
     </li>
   )
